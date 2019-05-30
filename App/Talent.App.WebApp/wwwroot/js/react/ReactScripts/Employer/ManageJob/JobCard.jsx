7c571e6a7b29f36ecee28adf6c55cfc43e58cb3c
ashnilp <ashnilp@gmail.com>
2019-05-30 23:38:33 +1200

    test

---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobCard.jsx
@@ -0,0 +1,156 @@
+﻿import React from 'react';
+import Cookies from 'js-cookie';
+import { Card, Button } from 'semantic-ui-react';
+import { ChildSingleInput } from '../../Form/SingleInput.jsx';
+
+export default class JobCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const details = props.details ? Object.assign({}, props.details) :
+            {
+                description: "",
+                location: "",
+                title: ""
+            }
+
+        this.state = {
+            showEditSection: false,
+            newDetails: details
+        }
+
+        this.openEdit = this.openEdit.bind(this)
+        this.closeEdit = this.closeEdit.bind(this)
+        this.closeEdit = this.closeEdit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.saveContact = this.saveContact.bind(this)
+        this.renderEdit = this.renderEdit.bind(this)
+        this.renderDisplay = this.renderDisplay.bind(this)
+
+    }
+
+    openEdit() {
+        const details = Object.assign({}, this.props.details) 
+        this.setState({
+            showEditSection: true,
+            newDetails: details
+        })
+    }
+
+    closeEdit() {
+        this.setState({
+            showEditSection: false
+        })
+    }
+
+    handleChange(event) {
+        const data = Object.assign({}, this.state.newDetails)
+        data[event.target.name] = event.target.value
+        this.setState({
+            newDetails: data
+        })
+    }
+
+
+    saveContact() {
+        console.log(this.props.details._id)
+        const data = Object.assign({}, this.state.newDetails)
+        this.props.controlFunc(this.props.details._id, data)
+        this.closeEdit()
+    }
+
+
+    render() {
+        return (
+            this.state.showEditSection ? this.renderEdit() : this.renderDisplay()
+        )
+    }
+
+    renderEdit() {
+        return (
+            <div className="ui cards">
+                <div className="card" >
+                    <div className="content">
+                        <div className='ui four wide column'>
+                            <ChildSingleInput
+                                inputType="text"
+                                label="Title"
+                                name="title"
+                                value={this.state.newDetails.title}
+                                controlFunc={this.handleChange}
+                                maxLength={80}
+                                placeholder="Enter your title"
+                                errorMessage="Please enter a valid title"
+                            />
+                            <ChildSingleInput
+                                inputType="text"
+                                label="Location"
+                                name="location"
+                                value={this.state.newDetails.location}
+                                controlFunc={this.handleChange}
+                                maxLength={80}
+                                placeholder="Enter your location"
+                                errorMessage="Please enter a valid location"
+                            />
+                            <ChildSingleInput
+                                inputType="text"
+                                label="Description"
+                                name="description"
+                                value={this.state.newDetails.description}
+                                controlFunc={this.handleChange}
+                                maxLength={80}
+                                placeholder="Enter your description"
+                                errorMessage="Please enter a valid description"
+                            />
+                            <button type="button" className="mini ui primary basic button" onClick={this.saveContact}>Save</button>
+                            <button type="button" className="mini ui primary basic button" onClick={this.closeEdit}>Cancel</button>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+            
+        )
+    }
+    
+    renderDisplay() {
+
+        let title = this.props.details ? this.props.details.title : ""
+        let location = this.props.details ? this.props.details.location : ""
+        let description = this.props.details ? this.props.details.description : ""
+        
+        return (
+            <div className="ui cards">
+                <div className="card" >
+                    <div className="content">
+                        <div className="header">
+                            {title}
+                        </div>
+                        <div className="meta">
+                            {location}
+                        </div>
+                        <div className="description">
+                            {description}
+                            
+                        </div>
+                        <div className="extra content">
+                            
+                                <button className="mini ui red button">
+                                    Expired
+                                </button>
+                                
+                                <button className="mini ui primary basic button">
+                                    Close
+                                </button>
+                                <button type="button" className="mini ui primary basic button" onClick={this.openEdit}>Edit</button>
+                                <button className="mini ui primary basic button">
+                                    Copy
+                                </button>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+}
---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
@@ -0,0 +1,181 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Cookies from 'js-cookie';
+import LoggedInBanner from '../../Layout/Banner/LoggedInBanner.jsx';
+import { LoggedInNavigation } from '../../Layout/LoggedInNavigation.jsx';
+import { JobSummaryCard } from './JobSummaryCard.jsx';
+import { BodyWrapper, loaderData } from '../../Layout/BodyWrapper.jsx';
+import JobCard from './JobCard.jsx';
+import { Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment, Card, List } from 'semantic-ui-react';
+
+export default class ManageJob extends React.Component {
+    constructor(props) {
+        super(props);
+        let loader = loaderData
+        loader.allowedUsers.push("Employer");
+        loader.allowedUsers.push("Recruiter");
+        //console.log(loader)
+        this.state = {
+            loadJobs: [],
+            loaderData: loader,
+            activePage: 1,
+            sortBy: {
+                date: "desc"
+            },
+            filter: {
+                showActive: true,
+                showClosed: false,
+                showDraft: true,
+                showExpired: true,
+                showUnexpired: true
+            },
+            totalPages: 1,
+            activeIndex: ""
+        }
+        this.loadData = this.loadData.bind(this);
+        this.init = this.init.bind(this);
+        this.loadNewData = this.loadNewData.bind(this);
+        //your functions go here
+
+        this.updateForComponentId = this.updateForComponentId.bind(this);
+        this.updateAndSaveData = this.updateAndSaveData.bind(this);
+        this.saveData = this.saveData.bind(this);
+    };
+
+    init() {
+       
+        let loaderData = TalentUtil.deepCopy(this.state.loaderData)
+        loaderData.isLoading = true;
+     
+        //this.setState({ loaderData });//comment this
+
+        //set loaderData.isLoading to false after getting data
+        this.loadData(() =>
+            this.setState({ loaderData : false })
+        )
+        
+        //console.log(this.state.loaderData)
+    }
+
+    componentDidMount() {
+        this.init();
+        loaderData.isLoading = false;
+    };
+
+    loadData(callback) {
+        
+        var link = 'http://localhost:51689/listing/listing/getSortedEmployerJobs';
+        var cookies = Cookies.get('talentAuthToken');
+       // your ajax call and other logic goes here
+        // ******* Ashnil ******
+
+        $.ajax({
+            url: 'http://localhost:51689/listing/listing/getSortedEmployerJobs',
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "GET",
+            contentType: "application/json",
+            dataType: "json",
+            data: {
+                activePage: 1,
+                sortbyDate: "desc",
+                showActive: true,
+                showClosed: false,
+                showDraft: true,
+                showExpired: true,
+                showUnexpired: true,
+                employerId: "",
+                limit: 6,
+               
+            },
+            success: function (data) {
+                this.setState({
+                    loadJobs: data.myJobs
+                });
+                
+            }.bind(this),
+            error: function (res) {
+                console.log(res.status)
+            }
+        }) 
+        
+        //**********************
+
+       
+    }
+
+    updateForComponentId(componentId, newValues) {
+        let data = {};
+        data[componentId] = newValues;
+        this.updateAndSaveData(data)
+    }
+
+    updateAndSaveData(newData) {
+        let newSD = Object.assign({}, this.state.employerData, newData)
+        this.setState({
+            employerData: newSD
+        }, this.saveData)
+    }
+
+    saveData() {
+
+
+    }
+
+    loadNewData(data) {
+        
+        var loader = this.state.loaderData;
+        loader.isLoading = true;
+        data[loaderData] = loader;
+        this.setState(data, () => {
+            this.loadData(() => {
+                loader.isLoading = false;
+                this.setState({
+                    loadData: loader
+                })
+            })
+        });
+        console.log(data);
+    }
+
+    render() {
+
+        let details = ""
+        if (this.state == "" && this.state.loadJobs == "") {
+            details = <p>No Jobs Found</p>
+        }
+        else {
+            details = this.state && this.state.loadJobs && this.state.loadJobs.map(job =>
+
+                <div className="ui horizontal list">
+                    <div className="item">
+                        <JobCard
+                            details={job}
+                            controlFunc={this.updateForComponentId}
+                        />
+                        <span></span>
+                    </div>
+
+                </div> 
+            )
+        }
+        
+    
+       return (
+            <BodyWrapper reload={this.init} loaderData={this.state.loaderData} >
+                <div className="ui container">
+                    <h1>List Of Jobs</h1>
+                   <div >
+                       {details}
+                    </div>
+                </div>
+            </BodyWrapper>
+        )
+            
+
+        
+        
+    }
+}
